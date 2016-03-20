e6d83b3607453142856fa4c84d78653b81b4ec89
ZIM <zim555@mail.ru>
2016-03-20 11:43:15 +0200

    Add other configs and source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 LOCAL_PATH := device/samsung/fortunave3g
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := fortunave3g,fortuna3g
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
@@ -16,3 +19,154 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_CORTEX_A53 := true
+
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK      := $(LOCAL_PATH)/mkbootimg.mk
+BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_BASE            := 0x80008000
+BOARD_RAMDISK_OFFSET         := 0x82000000
+BOARD_KERNEL_TAGS_OFFSET     := 0x81E00000
+BOARD_KERNEL_SEPARATED_DT    := true
+BOARD_KERNEL_PAGESIZE        := 2048
+TARGET_KERNEL_SOURCE         := kernel/samsung/fortunaxx3g
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_CONFIG         := msm8916_sec_defconfig
+TARGET_KERNEL_TIMA_CONFIG    := tima_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortunave3g_eur_defconfig
+
+# Partition sizes
+TARGET_USERIMAGES_USE_EXT4         := true
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1568669696
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 314572800
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+# (5731495936 - 16384)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5731479552
+BOARD_FLASH_BLOCK_SIZE             := 131072
+
+# Wifi
+BOARD_HAS_QCOM_WLAN 		     := true
+BOARD_HAS_QCOM_WLAN_SDK 	     := true
+BOARD_HOSTAPD_DRIVER 		     := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB 	     := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE 		         := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER 	 := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+TARGET_PROVIDES_WCNSS_QMI        := true
+TARGET_USES_QCOM_WCNSS_QMI 	     := true
+TARGET_USES_WCNSS_CTRL 		     := true
+WIFI_DRIVER_FW_PATH_AP 		     := "ap"
+WIFI_DRIVER_FW_PATH_STA 	     := "sta"
+WPA_SUPPLICANT_VERSION 		     := VER_0_8_X
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Custom RIL class
+BOARD_RIL_CLASS := ../../../device/samsung/fortunave3g/ril/
+PROTOBUF_SUPPORTED := true
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# malloc implementation
+MALLOC_IMPL := dlmalloc
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+
+# Charger
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Enable QCOM FM feature
+TARGET_QCOM_NO_FM_FIRMWARE := true
+AUDIO_FEATURE_ENABLED_FM := true
+
+# Enable HW based full disk encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Build our own PowerHAL
+TARGET_POWERHAL_VARIANT := qcom
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+#BOARD_VOLD_MAX_PARTITIONS 			:= 28
+
+# Camera
+TARGET_PROVIDES_CAMERA_HAL := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CMHW
+BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw
+
+# Workaround to avoid issues with legacy liblights on QCOM platforms
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Qcom
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_USES_QCOM_BSP := true
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := caf
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Display
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    bluetooth_loader.te \
+    file_contexts \
+    mediaserver.te \
+    property_contexts \
+    system_app.te \
+    time_daemon.te \
+    vold.te \
+    bluetooth.te \
+    file.te \
+    kernel.te \
+    mm-qcamerad.te \
+    property.te \
+    rild.te \
+    system_server.te \
+    ueventd.te \
+    wcnss_service.te
+	
+# Recovery
+TARGET_RECOVERY_FSTAB 				:= $(LOCAL_PATH)/rootdir/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 			:= true
+BOARD_HAS_LARGE_FILESYSTEM			:= true
+TARGET_RECOVERY_DENSITY 			:= hdpi	
+	
+# Misc.
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+# Dex
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
