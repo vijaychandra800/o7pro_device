449bf2777de066f35756ac7621392d428c88ed2e
ZIM <zim555@mail.ru>
2016-03-29 12:56:00 +0300

    Update kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,16 +25,13 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK      := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_BASE            := 0x80008000
-BOARD_RAMDISK_OFFSET         := 0x82000000
-BOARD_KERNEL_TAGS_OFFSET     := 0x81E00000
-BOARD_KERNEL_SEPARATED_DT    := true
+BOARD_KERNEL_BASE            := 0x80000000
 BOARD_KERNEL_PAGESIZE        := 2048
+BOARD_KERNEL_SEPARATED_DT    := true
+BOARD_KERNEL_TAGS_OFFSET     := 0x01e00000
+BOARD_RAMDISK_OFFSET         := 0x02000000
 TARGET_KERNEL_SOURCE         := kernel/samsung/fortunaxx3g
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
-TARGET_KERNEL_CONFIG         := msm8916_sec_defconfig
-TARGET_KERNEL_TIMA_CONFIG    := tima_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortunave3g_eur_defconfig
+TARGET_KERNEL_CONFIG         := fortunave3g_eur_cm_defconfig
 
 # Partition sizes
 TARGET_USERIMAGES_USE_EXT4          := true
