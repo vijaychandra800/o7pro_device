f7da2626f7805cb48995f22fb23b60779cf4780f
ZIM <zim555@mail.ru>
2016-03-30 16:08:17 +0300

    Update to kitkat kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,14 +32,17 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK      := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_BASE            := 0x80000000
+BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_TAGS_OFFSET     := 0x01E00000
+BOARD_RAMDISK_OFFSET         := 0x02000000
+BOARD_KERNEL_BASE 	     := 0x80000000
 BOARD_KERNEL_PAGESIZE        := 2048
 BOARD_KERNEL_SEPARATED_DT    := true
-BOARD_KERNEL_TAGS_OFFSET     := 0x01e00000
-BOARD_RAMDISK_OFFSET         := 0x02000000
+
 TARGET_KERNEL_SOURCE         := kernel/samsung/fortunaxx3g
-TARGET_KERNEL_CONFIG         := cyanogen_fortunave3g_defconfig
+#TARGET_KERNEL_CONFIG         := cyanogen_fortunave3g_defconfig
+TARGET_KERNEL_CONFIG          := cyanogenmod_fortunave3g_defconfig
+TARGET_KERNEL_SELINUX_CONFIG  := selinux_defconfig
 
 # Partition sizes
 TARGET_USERIMAGES_USE_EXT4          := true
