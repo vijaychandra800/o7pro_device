8a48be0b47379903e78e36077b9a79a95ffe000a
ZIM <zim555@mail.ru>
2016-03-31 22:18:32 +0300

    Add need rc

--- device_fortunave3g.mk
+++ device_fortunave3g.mk
@@ -35,6 +35,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
 	$(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
 	$(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.carrier.rc:root/init.carrier.rc \
 	$(LOCAL_PATH)/rootdir/init.class_main.sh:root/init.class_main.sh \
 	$(LOCAL_PATH)/rootdir/init.mdm.sh:root/init.mdm.sh \
 	$(LOCAL_PATH)/rootdir/init.qcom.bms.sh:root/init.qcom.bms.sh \
