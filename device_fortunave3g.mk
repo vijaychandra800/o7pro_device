49c7655f43bc6e7a14243d15ddb1816705f88dec
ZIM <zim555@mail.ru>
2016-04-05 11:53:35 +0300

    Update device_fortunave3g.mk

--- device_fortunave3g.mk
+++ device_fortunave3g.mk
@@ -35,6 +35,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
 	$(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
 	$(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.qcom.factory.sh:root/init.qcom.factory.sh \
 	$(LOCAL_PATH)/rootdir/etc/init.crda.sh:system/etc/init.crda.sh \
 	$(LOCAL_PATH)/rootdir/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
 	$(LOCAL_PATH)/rootdir/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
