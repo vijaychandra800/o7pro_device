bf459ceb5ead94f6484974d1b9023eee980c980f
ZIM <zim555@mail.ru>
2016-07-10 16:59:06 +0300

    Rebuild camera wrapper

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -113,16 +113,13 @@ static char *camera_fixup_getparams(int id, const char *settings)
 
     // fix params here
     params.set(android::CameraParameters::KEY_SUPPORTED_ISO_MODES, iso_values[id]);
-    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.1");
-    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-20");
-    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "20");
+    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.5");
+    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-2");
+    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "2");
 	
-	// fix params here
-	params.set("preview-format-values", "yuv420p");
-	params.set("video-frame-format", "yuv420p");
-	params.set("whitebalance-values", "auto");
-	params.set("auto-whitebalance-lock-supported", "false");
-
+	params.set("whitebalance-values", "auto,incandescent,fluorescent,daylight,cloudy-daylight");
+	params.set("effect-values", "none,mono,negative,sepia");
+	
     /* If the vendor has HFR values but doesn't also expose that
      * this can be turned off, fixup the params to tell the Camera
      * that it really is okay to turn it off.
@@ -168,7 +165,11 @@ static char *camera_fixup_setparams(struct camera_device *device, const char *se
         else if (strcmp(isoMode, "ISO800") == 0)
             params.set(android::CameraParameters::KEY_ISO_MODE, "800");
     }
-
+	
+	// fix params here
+	params.set("preview-format", "yuv420p");
+	params.set("video-frame-format", "yuv420p");
+	
     android::String8 strParams = params.flatten();
 
     if (fixed_set_params[id])
