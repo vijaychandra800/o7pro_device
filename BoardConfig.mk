777833282572adb991052ed4670cf2dc1d6f0cda
ZIM <zim555@mail.ru>
2016-08-21 00:21:08 +0300

    Del not used mk file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,6 @@ TARGET_USES_NEW_ION_API              :=true
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
-#BOARD_CUSTOM_BOOTIMG_MK      := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_ARCH           := arm
 BOARD_DTBTOOL_ARG            := -2
 BOARD_KERNEL_BASE            := 0x80000000
