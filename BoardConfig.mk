23198ffc66f05ced66b23b8852c334da7b7cd7bc
ZIM <zim555@mail.ru>
2016-09-14 19:41:33 +0300

    Fix time reset and clean init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,8 @@ TARGET_PLATFORM_DEVICE_BASE          := /devices/soc.0/
 HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE   := true
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY := true
 TARGET_USES_QCOM_BSP                 := true
-TARGET_USES_NEW_ION_API              :=true
+TARGET_USES_NEW_ION_API              := true
+BOARD_USES_QC_TIME_SERVICES          := true
 
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
