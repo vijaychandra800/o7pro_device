889d94ec800a33b3516b59cdde2f6804f1365e09
ZIM <zim555@mail.ru>
2016-11-09 19:54:16 +0200

    Update camera

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -139,7 +139,8 @@ static char *camera_fixup_getparams(int id, const char *settings)
         isVideo = !strcmp(params.get(android::CameraParameters::KEY_RECORDING_HINT), "true");
 
     if(!isVideo){
-	params.set("auto-exposure-values", "center");
+		params.set("auto-exposure-values", "center");
+		params.set("preview-format-values", "yuv420p");
     }
 	
     android::String8 strParams = params.flatten();
@@ -177,11 +178,11 @@ static char *camera_fixup_setparams(struct camera_device *device, const char *se
             params.set(android::CameraParameters::KEY_ISO_MODE, "800");
     }
 	
-    int video_width, video_height;
-    params.getPreviewSize(&video_width, &video_height);
-    if(video_width*video_height <= 960*540){
-	params.set("preview-format", "yuv420p");
-    }
+    //int video_width, video_height;
+    //params.getPreviewSize(&video_width, &video_height);
+    //if(video_width*video_height <= 960*540){
+	//	params.set("preview-format", "yuv420p");
+    //}
 	
     android::String8 strParams = params.flatten();
 
