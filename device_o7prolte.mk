27ac29421526daf160f2f755405c933720ea3631
grace5921 <androidlover5842@gmail.com>
2017-03-08 11:34:44 +0530

    Revert "use default value of ro.adb.secure"
    This reverts commit 4c67d6b523d898384dd30ad295e94f12d41b40de.

--- device_o7prolte.mk
+++ device_o7prolte.mk
@@ -229,6 +229,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
+    ro.adb.secure=0 \
     ro.debuggable=1 \
     persist.service.adb.enable=1
 
